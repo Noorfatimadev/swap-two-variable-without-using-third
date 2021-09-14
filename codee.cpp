4b120df2a32b927a30562f98c2880ea2c1c61baf
Noorfatimadev <59045813+Noorfatimadev@users.noreply.github.com>
2021-09-14 17:14:15 +0500

    Add files via upload

---
+++ codee.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+int a, b;   
+ cout<<"enter a"<<endl;
+ cin>>a;
+ cout<<"enter b"<<endl;
+cin>>b;
+cout<<"a= "<<a<<" b= "<<b<<endl;      
+a=a+b;     
+b=a-b;   
+a=a-b;     
+cout<<"After swap a= "<<a<<" b= "<<b<<endl;      
+return 0;  
+} 
